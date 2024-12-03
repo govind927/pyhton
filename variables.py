a7b36f8dbf7f83122d8919f97a6bab78fc0a55de
govind927 <govindsharma0476@gmail.com>
2024-12-03 11:56:06 +0530

    Chapter 2(H)

---
+++ variables.py
@@ -0,0 +1,52 @@
+a = 1   # a is a Variable of int data type
+
+b = 1.08   # b is a Variable of Float data type
+
+c = "Govind"  # c is a Variable of String data type
+
+d = True      # d is a Variable of Boolean data type
+
+e = None  # e is a Variable of None data type
+
+# Operators are of four types Assignment , Logical , Comparision and Logical
+
+# Assignment : += , -= , = etc.
+# Logical : and or not
+# Comparision: == , != , < , > , <= , >= etc.
+# Arithmatic : + , - , * , / etc.
+
+# print(a * b)
+# print(c)
+# print(d)
+
+# print(type(a))
+
+# print(type(b))
+
+# print(type(c))
+
+# print(type(d))
+
+# print(str(32))
+
+# print(float(32))
+
+#Taking input from user
+
+# a = int(input("Enter a number : "))
+
+# b= int(input("Enter second number : "))
+
+# print("The sum of a and b is = " , a + b)
+
+# a = int(input("Enter any number : "))
+
+# z = int(input("Enter another number : "))
+
+# print("The average of a and Z is : " , (a+z) / 2 )
+
+# Square of a number enter by user
+
+a = int(input("Enter number to find its square : "))
+
+print("The square of" , a ,"is : " , a**2)
