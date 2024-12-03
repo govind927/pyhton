d52383dc867d689536c8e4e112c21b8b9197292d
govind927 <govindsharma0476@gmail.com>
2024-12-03 14:08:39 +0530

    Python Strings

---
+++ strings.py
@@ -0,0 +1,35 @@
+# String is an inmutable data types that stores values in quotes
+str = "   Hello everyone my name is Govind Sharma I am a 2nd year Engineering student "
+
+# print(str[-4 : -1])
+
+# print(str[1 : 4])
+
+# print(str.endswith("78"))
+
+# print(len(str))
+
+# print(str.upper())
+
+# print(str.lower())
+
+# print(str.title())
+
+# print(str.strip())
+
+# print(str.split(","))
+
+# print(str.replace("second" , "2nd"))
+
+# print(str.find("."))
+
+# print(str.find("9"))
+
+# print(str.count("E"))
+
+# print(str.isalnum())
+
+#Program to detect double space in a String
+# str = "we will detect  double space  in this string"
+# print(str.find("  "))  # strings are immutable these functions generate new strings and print it ,
+                        #don't change orignal string
